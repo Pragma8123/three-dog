fa8db3e41bf95ca434207d082b86f29883b42136
Max Machon <max.machon@3xlogic.com>
2019-01-14 17:01:14 -0500

    Add some stats the "control panel"

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,58 @@
   <section class="hero is-fullheight is-dark">
     <div class="hero-body">
       <div class="container has-text-centered">
-        <div class="level">
-          <div class="level-item">
-            <figure class="image is-128x128">
-              <img src="gnrlogo.png">
-            </figure>
+        <div class="columns is-centered">
+          <div class="column is-6">
+            <div class="level">
+              <div class="level-item">
+                <figure class="image is-128x128">
+                  <img src="gnrlogo.png">
+                </figure>
+              </div>
+            </div>
+            <h1 class="title is-1">THREE DOG</h1>
+            <h4 class="subtitle is-4">
+              <i>A Fallout Themed Discord Radio Bot</i>
+            </h4>
+            <discord-stats-level :guilds="guilds" :streams="streams" :uptime="uptime"></discord-stats-level>
           </div>
         </div>
-        <h1 class="title is-1">Three Dog</h1>
-        <h4 class="subtitle is-4">A Fallout themed Discord radio bot</h4>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-export default { name: 'App' };
+import axios from 'axios';
+import DiscordStatsLevel from './components/DiscordStatsLevel.vue';
+
+export default {
+  name: 'App',
+  components: { DiscordStatsLevel },
+  data() {
+    return {
+      guilds: 0,
+      streams: 0,
+      uptime: 0,
+    };
+  },
+  mounted() {
+    axios
+      .get('/api/guilds')
+      .then(res => (this.guilds = res.data))
+      .catch(err => console.log(err));
+    axios
+      .get('/api/streams')
+      .then(res => (this.streams = res.data))
+      .catch(err => console.log(err));
+    axios
+      .get('/api/uptime')
+      .then(res => (this.uptime = res.data))
+      .catch(err => console.log(err));
+  },
+};
 </script>
 
 <style lang="scss">
-@import "styles.scss";
+@import 'styles.scss';
 </style>
---
+++ src/components/DiscordStatsLevel.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="level">
+    <div class="level-item has-text-centered">
+      <div>
+        <p class="heading has-text-success">Guilds</p>
+        <p class="title">{{ guilds }}</p>
+      </div>
+    </div>
+    <div class="level-item has-text-centered">
+      <div>
+        <p class="heading has-text-success">Listeners</p>
+        <p class="title">{{ streams }}</p>
+      </div>
+    </div>
+    <div class="level-item has-text-centered">
+      <div>
+        <p class="heading has-text-success">Uptime</p>
+        <p class="title is-4">{{ uptimePretty }}</p>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+  name: 'DiscordStatsLevel',
+  props: {
+    guilds: { type: Number, default: 0 },
+    streams: { type: Number, default: 0 },
+    uptime: { type: Number, default: 0 },
+  },
+  computed: {
+    uptimePretty() {
+      const temp = moment.duration(this.uptime);
+      return `${temp.hours()}h:${temp.minutes()}m`;
+    },
+  },
+};
+</script>

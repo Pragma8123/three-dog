a8534e488d4f72dd7032c4f2314d7a7f58a5e01f
Max Machon <max.machon@3xlogic.com>
2019-01-17 23:08:40 -0500

    Make sure knex defaults to dev config

--- knex.js
+++ knex.js
@@ -2,4 +2,4 @@ require('dotenv').config(); // Load env variables
 const knexConfig = require('./knexfile');
 const knex = require('knex');
 
-module.exports = knex(knexConfig[process.env.NODE_ENV]);
+module.exports = knex(knexConfig[process.env.NODE_ENV || 'development']);

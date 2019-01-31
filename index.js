96e080dc7c57c2e021bebce75ef967a5ccc61931
pragma <pragma8123@gmail.com>
2019-01-31 08:55:58 -0500

    Move website to separate repository

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 require('dotenv').config(); // Load env variables from .env
-const Bundler = require('parcel-bundler');
-const { join } = require('path');
-const fs = require('fs');
 const Eris = require('eris');
-const app = require('express')();
-const compression = require('compression');
 const dbl = require('./dbl');
-const api = require('./api');
 const { onReady, onMessageCreate } = require('./handlers');
 const logger = require('./logger');
 
@@ -17,24 +11,3 @@ bot.sharedStream = new Eris.SharedStream();
 bot.on('ready', err => onReady(bot, err));
 bot.on('messageCreate', msg => onMessageCreate(bot, msg));
 bot.connect();
-
-// Register bot with express so we can access it in API
-app.set('bot', bot);
-app.use('/api', api);
-app.use(compression());
-
-// Parcel bundler middleware
-const bundler = new Bundler(join(__dirname, 'index.html'));
-bundler.on('buildEnd', () => {
-  // Copy threedog.png for Open Graph metadata
-  fs.copyFileSync(
-    join(__dirname, 'src/threedog.png'),
-    join(__dirname, 'dist/threedog.png')
-  );
-});
-app.use(bundler.middleware());
-
-const port = process.env.PORT || 8080;
-app.listen(port, err => {
-  if (err) logger.error(null, err);
-});

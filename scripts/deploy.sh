96414bf8cb141bc16a398db5a09633290250ddbb
pragma <pragma8123@gmail.com>
2019-02-01 11:09:37 -0500

    .

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 eval "$(ssh-agent -s)" # start ssh agent
-chmod 600 .travis/deploy.key # key should have push access
-ssh-add .travis/deploy.key
+chmod 600 deploy.key # key should have push access
+ssh-add deploy.key
 ssh-keyscan dokku@192.241.145.31 >> ~/.ssh/known_hosts
 git remote add deploy dokku@192.241.145.31:three-dog-bot
 git config --global push.default simple

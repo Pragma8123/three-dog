c76c5933e3441492d553b61d77f6e869364e6b7d
pragma <pragma8123@gmail.com>
2019-02-01 14:08:04 -0500

    .

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 eval "$(ssh-agent -s)" # start ssh agent
+ls -lah # debug
 chmod 600 deploy.key # key should have push access
 ssh-add deploy.key
 ssh-keyscan 192.241.145.31 >> ~/.ssh/known_hosts

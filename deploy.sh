c8658f9bdcd35453993a3732c09448bccf6266ad
pragma <pragma8123@gmail.com>
2019-02-01 14:13:31 -0500

    .

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+openssl aes-256-cbc -K $encrypted_b7ccb24178b3_key -iv $encrypted_b7ccb24178b3_iv -in deploy.key.enc -out deploy.key -d
 eval "$(ssh-agent -s)" # start ssh agent
 ls -lah # debug
 chmod 600 deploy.key # key should have push access

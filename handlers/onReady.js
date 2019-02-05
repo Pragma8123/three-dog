4b31cda958d8232a79ba386e8aac9e2603ebfe02
Max Machon <max.machon@3xlogic.com>
2019-02-04 23:13:42 -0500

    Add DDBL API and refactor bot list API hooks

--- handlers/onReady.js
+++ handlers/onReady.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const { join } = require('path');
+const DBL = require('dblapi.js');
+const DDBL = require('ddblapi.js');
 const logger = require('../logger');
 
 module.exports = (bot, err) => {
@@ -17,5 +19,19 @@ module.exports = (bot, err) => {
       // We should restart the stream when it ends
       bot.sharedStream.play(join(__dirname, '..', 'gnr.ogg'));
     });
+
+    // === Connect to bot list APIs ===
+    if (process.env.NODE_ENV === 'production') {
+      // Discord bot list
+      const dbl = new DBL(process.env.DBL_TOKEN, bot);
+      dbl.on('posted', () => logger.verbose('DBL: Server count posted'));
+      dbl.on('error', err => logger.error(null, err));
+      bot.dbl = dbl;
+
+      // Devine discord bot list
+      const ddbl = new DDBL(process.env.DDBL_TOKEN);
+      ddbl.postStats(bot.user.id, bot.guilds.size);
+      bot.ddbl = ddbl;
+    }
   }
 };
--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-require('dotenv').config(); // Load env variables from .env
+require('dotenv').config();
 const Eris = require('eris');
-const dbl = require('./dbl');
 const { onError, onWarn, onReady, onMessageCreate } = require('./handlers');
 
-const bot = new Eris(process.env.BOT_TOKEN, {
-  compress: true,
-  opusOnly: true,
-});
-bot.dbl = dbl(bot, process.env.DBL_TOKEN); // Discord Bot List API
+const bot = new Eris(process.env.BOT_TOKEN, { compress: true, opusOnly: true });
 bot.sharedStream = new Eris.SharedStream();
 
 bot.on('error', (err, id) => onError(bot, err, id));
 bot.on('warn', (message, id) => onWarn(bot, message, id));
 bot.on('ready', err => onReady(bot, err));
 bot.on('messageCreate', msg => onMessageCreate(bot, msg));
+
 bot.connect();

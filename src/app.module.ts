ffd6258a39d443d439d298126d073c38bced28f9
Max Machon <max@machon.family>
2022-11-28 15:17:27 -0500

    feat(client): init client

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,18 @@
 import { DiscordModule } from '@discord-nestjs/core';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { GatewayIntentBits } from 'discord.js';
-import { AppController } from './app.controller';
+import { join } from 'path';
 import { BotModule } from './bot/bot.module';
 import { RedditModule } from './reddit/reddit.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'client', 'dist'),
+    }),
     DiscordModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
@@ -29,6 +33,5 @@ import { RedditModule } from './reddit/reddit.module';
     BotModule,
     RedditModule,
   ],
-  controllers: [AppController],
 })
 export class AppModule {}

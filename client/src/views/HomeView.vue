bab8d53e54bcb875dcc8648595e75d1653fd1ea3
Max Machon <max@machon.family>
2022-12-02 18:26:20 -0500

    feat(client): adding Vue to the front end

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <div class="columns is-centered">
+        <div class="column is-6">
+          <div class="level">
+            <div class="level-item">
+              <figure class="image is-128x128">
+                <img src="/gnrlogo.png" />
+              </figure>
+            </div>
+          </div>
+          <h1 class="title is-1 is-uppercase">Three Dog</h1>
+          <h4 class="subtitle is-4">
+            <i>A Fallout-Themed Discord Bot</i>
+          </h4>
+        </div>
+      </div>
+      <div class="columns is-centered is-mobile">
+        <div class="column is-6">
+          <a
+            class="button is-medium is-success"
+            target="_blank"
+            href="https://discordapp.com/oauth2/authorize?client_id=461602422192734228&scope=bot&permissions=3148800"
+            >Invite Me</a
+          >
+        </div>
+      </div>
+      <div class="columns is-centered is-mobile">
+        <div class="column is-6">
+          <div class="content has-text-centered">
+            <div class="buttons has-addons is-centered">
+              <a
+                class="button"
+                target="_blank"
+                href="https://paypal.me/pragma8123"
+                >Donate</a
+              >
+              <a
+                class="button"
+                target="_blank"
+                href="https://discord.gg/QwfXED8"
+              >
+                <span class="icon is-small">
+                  <i class="fab fa-discord"></i>
+                </span>
+              </a>
+              <a
+                class="button"
+                target="_blank"
+                href="https://github.com/Pragma8123/3dog.me"
+              >
+                <span class="icon is-small">
+                  <i class="fab fa-github"></i>
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>

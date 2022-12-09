442b5d9e22183bdd33f51f478a550e8d93089973
Max Machon <max@machon.family>
2022-12-09 12:13:00 -0500

    fix(oauth): Handle invalid code errors gracefully

--- src/auth/guards/discord.guard.ts
+++ src/auth/guards/discord.guard.ts
@@ -1,5 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class DiscordAuthGuard extends AuthGuard('discord') {}
+export class DiscordAuthGuard extends AuthGuard('discord') {
+  private readonly logger = new Logger(DiscordAuthGuard.name);
+
+  handleRequest(err: any, user: any, info: any) {
+    if (err && err.name === 'TokenError') {
+      this.logger.error(err);
+      throw new BadRequestException(err.message);
+    }
+
+    return user;
+  }
+}

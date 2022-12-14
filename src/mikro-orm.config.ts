215ba6548de4bbe0024e0933b64922c9ef665c89
Max Machon <max@machon.family>
2022-12-14 16:55:49 -0500

    build(db): Fix issue with migration creation
    Paths in MikroORM config were not relative preventing ts-morph from doing its thang

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -3,12 +3,12 @@ import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
 const config = {
   type: 'sqlite',
   dbName: 'db/three-dog.sqlite3',
-  entities: ['dist/**/*.entity.js'],
-  entitiesTs: ['src/**/*.entity.ts'],
+  entities: ['./dist/**/*.entity.js'],
+  entitiesTs: ['./src/**/*.entity.ts'],
   metadataProvider: TsMorphMetadataProvider,
   migrations: {
-    path: 'dist/migrations',
-    pathTs: 'src/migrations',
+    path: './dist/migrations',
+    pathTs: './src/migrations',
   },
 };
 

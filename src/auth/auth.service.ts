bf4812b9158e286bd5cff6f5fb0cc7127ac9c7d5
Max Machon <max@machon.family>
2022-12-26 08:38:24 -0500

    style(lint): Remove unused imports

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { User } from '../users/entities/user.entity';
-import { CreateUserDto } from '../users/dto/create-user.dto';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 

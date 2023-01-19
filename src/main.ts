c388232719c241835cfbdc9a4893bbf5006fdd08
Max Machon <max@machon.family>
2023-01-19 09:05:21 -0500

    refactor(bot): remove unnecessary console log call

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix('api');
   process.on('SIGINT', async () => {
-    console.debug('SIGINT signal received: closing HTTP server');
     await app.close();
     process.exit(0);
   });
